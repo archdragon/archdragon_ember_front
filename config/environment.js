e737c4f8687f0d0500a308856d706886a9f38735
Przemek <przemek@krowinski.eu>
2015-05-20 19:51:40 +0200

    Temporarily loosen security policy

--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,15 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "*",
+      'font-src': "*",
+      'connect-src': "*",
+      'img-src': "*",
+      'style-src': "*",
+      'media-src': "*"
     }
   };
 
