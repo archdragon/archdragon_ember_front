921f0a01cd39a42761e5956056970ca309ade7c8
Przemek <przemek@krowinski.eu>
2015-05-23 18:42:51 +0200

    Add glyphicons font

--- Brocfile.js
+++ Brocfile.js
@@ -19,5 +19,7 @@ var app = new EmberApp({});
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {  
+    destDir: 'fonts/'
+});
 module.exports = app.toTree();
