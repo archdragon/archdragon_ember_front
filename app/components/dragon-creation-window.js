d0cee36644f8e727ee6b4a49fb9cedd8bc44fbab
Przemek <przemek@krowinski.eu>
2015-05-23 18:43:53 +0200

    Add scrollable dragon creation window

--- app/components/dragon-creation-window.js
+++ app/components/dragon-creation-window.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  scrollPosition: 0,
+  scrollMin: 0,
+  scrollMax: 2,
+  startCarousel: function() {
+  }.on('didInsertElement'),
+  scrollChanged: function() {
+    var scrollPosition = this.get('scrollPosition');
+    var leftOffset = scrollPosition * 100;
+    Ember.$('.scrollable-container').css('left', leftOffset + '%');
+  }.observes('scrollPosition'),
+  actions: {
+    scroll: function(direction) {
+      direction = parseInt(direction);
+      var scrollPosition = this.get('scrollPosition') + direction;
+      if(scrollPosition > this.get('scrollMax')) {
+        scrollPosition = this.get('scrollMin');
+      } else if (scrollPosition < this.get('scrollMin')) {
+        scrollPosition = this.get('scrollMax');
+      }
+
+      this.set('scrollPosition', scrollPosition);
+    }
+  }
 });
