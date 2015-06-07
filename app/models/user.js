61e0543ca71ee9b9d6810ab8a6e1f680dc15cede
Przemek <przemek@krowinski.eu>
2015-06-07 22:06:46 +0200

    Add user model

---
+++ app/models/user.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  email: DS.attr('string'),
+  password: DS.attr('string'),
+  password_confirmation: DS.attr('string'),
+  apiKeys: DS.hasMany('apiKey'),
+  errors: {}
+});
